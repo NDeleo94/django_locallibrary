4765e839dee0c09405cae692ad323cd7aa0af509
NDeleo94 <n.deleo94@gmail.com>
2022-01-06 20:51:30 -0300

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True #line for dev
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False' #line for prod
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tranquil-meadow-27172.herokuapp.com']
 
 
 # Application definition
